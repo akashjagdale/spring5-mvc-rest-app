02102beef8ef05455ef9b9a66258f19debf6a307
akashjagdale <adjagdale25@gmail.com>
2021-03-11 17:18:45 +0530

    Customizing Swagger with Meta Data

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -1,28 +1,52 @@
 package guru.springfamework.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig { //} extends WebMvcConfigurationSupport {
 
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
-/**
- * Use following code if not using spring boot.
- * Spring boot automatically configures swagger ui.
- */
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Akash Jagdale",
+                "",
+                "adjagdale25@gmail.com");
+
+        return new ApiInfo(
+                "Spring Boot Swagger Example App",
+                "This is example app for spring mvc rest api and swagger",
+                "1.0",
+                "Term of service: Ha ha",
+                contact,
+                "Apache License Version",
+                "https://www.apache.org/licnse/LICENSE-2.0",
+                new ArrayList<>());
+    }
+
+    /**
+     * Use following code if not using spring boot.
+     * Spring boot automatically configures swagger ui.
+     */
 //    @Override
 //    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("swagger-ui.html")
@@ -31,5 +55,6 @@ public class SwaggerConfig { //} extends WebMvcConfigurationSupport {
 //        registry.addResourceHandler("/webjars/**")
 //                .addResourceLocations("classpath:/META-INF/resources/webjars/");
 //    }
+
 
 }
--- target/generated-sources/annotations/guru/springfamework/api/v1/mapper/CategoryMapperImpl.java
+++ target/generated-sources/annotations/guru/springfamework/api/v1/mapper/CategoryMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-11T16:37:26+0530",
+    date = "2021-03-11T17:16:22+0530",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
